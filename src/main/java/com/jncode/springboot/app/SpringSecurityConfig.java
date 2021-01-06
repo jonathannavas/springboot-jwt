32bd8250eb43b9fef12515c73b64e65b40ca3883
Jonathan <jonathan.navas9@hotmail.com>
2021-01-06 17:49:20 -0500

    Implementacion de seguridad con JWT api rest clientes

--- src/main/java/com/jncode/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/jncode/springboot/app/SpringSecurityConfig.java
@@ -6,17 +6,20 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.jncode.springboot.app.auth.handler.LoginSuccessHandler;
+import com.jncode.springboot.app.auth.filter.JWTAuthenticationFilter;
+import com.jncode.springboot.app.auth.filter.JWTAuthorizationFilter;
+import com.jncode.springboot.app.auth.service.JWTService;
 import com.jncode.springboot.app.models.service.JpaUserDetailService;
 
 @EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private LoginSuccessHandler successHandler;
+	/*@Autowired
+	private LoginSuccessHandler successHandler;*/
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
@@ -24,25 +27,19 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JpaUserDetailService userDetailService;
 	
+	@Autowired
+	private JWTService jwtService;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeRequests().antMatchers("/","/css/**","/js/**","/listar","/locale","/listar-rest","/api/clientes/**").permitAll()
-		/*.antMatchers("/ver/**").hasAnyRole("USER")
-		.antMatchers("/uploads/**").hasAnyRole("USER")
-		.antMatchers("/form/**").hasAnyRole("ADMIN")
-		.antMatchers("/eliminar/**").hasAnyRole("ADMIN")
-		.antMatchers("/factura/**").hasAnyRole("ADMIN")*/
+		http.authorizeRequests().antMatchers("/","/css/**","/js/**","/listar**","/locale").permitAll()
 		.anyRequest().authenticated()
 		.and()
-			.formLogin()
-				.successHandler(successHandler)
-				.loginPage("/login")
-			.permitAll()
-		.and()
-		.logout().permitAll()
-		.and()
-		.exceptionHandling().accessDeniedPage("/error_403");
+		.addFilter(new JWTAuthenticationFilter(authenticationManager(),jwtService))
+		.addFilter(new JWTAuthorizationFilter(authenticationManager(),jwtService))
+		.csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
 
